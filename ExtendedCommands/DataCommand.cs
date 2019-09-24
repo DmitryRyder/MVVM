64a6b932a96a3668d91464d5cbb924c93f8ed7e3
Ryder <dmitryryder91@gmail.com>
2019-09-24 21:38:20 +0300

    init

---
+++ ExtendedCommands/DataCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ExtendedCommands
+{
+    public static class DataCommand
+    {
+        private static RoutedUICommand requery;
+
+        static DataCommand()
+        {
+            // Инициализация команды. 
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.R, ModifierKeys.Control, "Ctrl+R"));
+            requery = new RoutedUICommand("Requery", "Requery", typeof(DataCommand), inputs);
+        }
+        public static RoutedUICommand Requery
+        {
+            get { return requery; }
+        }
+    }
+}
