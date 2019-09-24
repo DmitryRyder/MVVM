f2414fe40392c3399aef8cc7c2d10da3d4748ca0
Ryder <dmitryryder91@gmail.com>
2019-09-24 21:36:55 +0300

    init

---
+++ ControlsApp/Window1.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ControlsApp
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void add_H_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string i = Convert.ToString(Wrap_H.Children.Count + 1);
+
+            SolidColorBrush[] colors = new SolidColorBrush[4] { Brushes.Blue, Brushes.Green, Brushes.White, Brushes.LawnGreen };
+
+            Random y = new Random();
+
+            int c = y.Next(0, 4);
+
+            Button l = new Button();
+
+            l.Content = i;
+            l.Width = 80;
+            l.Height = 30;
+            l.Background = colors[c];
+
+            Wrap_H.Children.Add(l);
+        }
+
+        private void del_H_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Wrap_H.Children.Count != 0)
+            {
+                Wrap_H.Children.RemoveAt((Wrap_H.Children.Count - 1));
+            }
+            else MessageBox.Show("Элементов для удаления не осталось");
+        }
+
+        private void add_V_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string i = Convert.ToString(Wrap_V.Children.Count + 1);
+
+            SolidColorBrush[] colors = new SolidColorBrush[4] { Brushes.Blue, Brushes.Green, Brushes.White, Brushes.LawnGreen };
+
+            Random y = new Random();
+
+            int c = y.Next(0, 4);
+
+            Button l = new Button();
+
+            l.Content = i;
+            l.Width = 80;
+            l.Height = 30;
+            l.Background = colors[c];
+
+            Wrap_V.Children.Add(l);
+        }
+
+        private void del_V_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Wrap_V.Children.Count != 0)
+            {
+                Wrap_V.Children.RemoveAt((Wrap_V.Children.Count - 1));
+            }
+            else MessageBox.Show("Элементов для удаления не осталось");
+        }
+    }
+}
---
+++ ControlsApp/Window2.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ControlsApp
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBox chbx = (CheckBox)sender;
+
+            chbx.Content = "Неотмечено";
+
+            sender = chbx;
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox chbx = (CheckBox)sender;
+
+            chbx.Content = "Отмечено";
+
+            sender = chbx;
+        }
+
+        private void checkBox_Indeterminate(object sender, RoutedEventArgs e)
+        {
+            CheckBox chbx = (CheckBox)sender;
+
+            chbx.Content = "Неопределено";
+
+            sender = chbx;
+        }
+
+        private void RadioButton_Checked1(object sender, RoutedEventArgs e)
+        {
+
+            RadioButton rdb = new RadioButton();
+            rdb = (RadioButton)sender;
+
+            foreach (UIElement p in StackPanel1.Children)
+            {
+                if (p is TextBlock) ((TextBlock)p).Text = "Выбрано " + rdb.Content.ToString();
+            }
+
+            rdb.IsChecked = true;
+
+            sender = rdb;
+        }
+        private void RadioButton_Checked2(object sender, RoutedEventArgs e)
+        {
+
+            RadioButton rdb = new RadioButton();
+            rdb = (RadioButton)sender;
+
+            foreach (UIElement p in StackPanel2.Children)
+            {
+                if (p is TextBlock) ((TextBlock)p).Text = "Выбрано " + rdb.Content.ToString();
+            }
+
+            rdb.IsChecked = true;
+
+            sender = rdb;
+        }
+
+        private void Button_MouseEnter_1(object sender, MouseEventArgs e)
+        {
+            popup1.IsOpen = true;
+        }
+
+        private void Up_Click(object sender, RoutedEventArgs e)
+        {
+            scroll.LineUp();
+        }
+
+        private void Down_Click(object sender, RoutedEventArgs e)
+        {
+            scroll.LineDown();
+        }
+    }
+}
